48bd4bdd8975b0a2e16e40772c52b5fa65d0267a
jvilla <josevillacinda96@gmail.com>
2021-04-07 08:50:05 -0700

    added mongodb_uri

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,26 @@
-self.addEventListener('install', event =>{
-  event.waitUntil(caches.open('budget-tracker-v1')
-  .then(cache => cache.addAll([
-    '/',
-    '/index.html',
-    'manifest.json',
-    '/icons/icon-512x512.png',
-    '/icons/icon-192x192.png'
-  ])))
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('Budget-cache-v1')
+    .then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json',
+      '/icons/icon-192x192.png',
+      '/icons/icon-512x512.png'
+    ])))
 })
 
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request)
-  .catch(err => {
-    if(err){ console.log(err)}
+    .catch(err => {
+      if (err) { console.log(err) }
 
-    return caches.match(event.request)
-     .then(match => {
-       if (match){
-         return match
-       } else if(event.request.headers.get('accept').includes('text.html')){
-         return caches.match('/')
-       }
-     })
-  }))
+      return caches.match(event.request)
+        .then(match => {
+          if (match) {
+            return match
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/')
+          }
+        })
+    }))
 })
